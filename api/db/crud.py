27b7e919852c8cbd150527e6373fecc3ec3c7bf0
Rokas Tarasevičius <rokas.tarasevicius@gmail.com>
2022-11-04 21:40:19 +0200

    feat: base devices added

--- api/db/crud.py
+++ api/db/crud.py
@@ -8,3 +8,22 @@ def create_device(db: Session, device: schemas.DeviceCreate):
     db.commit()
     db.refresh(db_device)
     return db_device
+
+def get_devices(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Device).offset(skip).limit(limit).all()
+
+def get_device(db: Session, device_id: int):
+    return db.query(models.Device).filter(models.Device.id == device_id).first()
+
+def update_device(db: Session, device_id: int, device: schemas.DeviceCreate):
+    db_device = models.Device(**device.dict())
+    db.add(db_device)
+    db.commit()
+    db.refresh(db_device)
+    return db_device
+
+def delete_device(db: Session, device_id: int):
+    db_device = db.query(models.Device).filter(models.Device.id == device_id).first()
+    db.delete(db_device)
+    db.commit()
+    return db_device
