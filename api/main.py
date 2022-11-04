db8f936bc8e8a60a6f0c682accd29caa8564c822
Rokas Tarasevičius <rokas.tarasevicius@gmail.com>
2022-11-04 21:50:05 +0200

    chore: version bump

--- api/main.py
+++ api/main.py
@@ -4,10 +4,11 @@ from api.endpoints import devices
 from db import models
 from db.database import engine
 from endpoints import stats
+import pkg_resources
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Power management API", description="API for the power management APP project")
+app = FastAPI(title="Power management API", description="API for the power management APP project", version=pkg_resources.get_distribution("power-manager-api").version)
 
 # Dependency
 app.include_router(stats.router)
